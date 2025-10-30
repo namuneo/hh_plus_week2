01c60abdfb0442858483426e208026a29e2ea153
namuneo <rlatjdwns8934@gmail.com>
2025-10-30 14:56:56 +0900

    first commit

---
+++ src/main/java/sample/hhplus_w2/HhplusW2Application.java
@@ -0,0 +1,13 @@
+package sample.hhplus_w2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhplusW2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HhplusW2Application.class, args);
+    }
+
+}
---
+++ src/test/java/sample/hhplus_w2/HhplusW2ApplicationTests.java
@@ -0,0 +1,13 @@
+package sample.hhplus_w2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhplusW2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
